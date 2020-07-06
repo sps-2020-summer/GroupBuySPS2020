806510a9fc3544208798bdfa565650166433fe5c
Eugene Teu <eugene.teu@shopee.com>
2020-07-06 22:00:57 +0800

    fix indent

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -3,18 +3,18 @@ import { Card, Button } from "antd";
 import * as s from "./login.module.css";
 
 const Login: FC<{}> = () => {
-  return (
-    <Card className={s["loginCard"]}>
-      <h1>Group Buy</h1>
-      <div className={s["actionArea"]}>
-        <Button type="primary">Google Login</Button>
-        {
-          // guest to login during testing
-        }
-        <Button type="dashed">Guest Login</Button>
-      </div>
-    </Card>
-  );
+	return (
+		<Card className={s["loginCard"]}>
+			<h1>Group Buy</h1>
+			<div className={s["actionArea"]}>
+				<Button type="primary">Google Login</Button>
+				{
+					// guest to login during testing
+				}
+				<Button type="dashed">Guest Login</Button>
+			</div>
+		</Card>
+	);
 };
 
 export default Login;
