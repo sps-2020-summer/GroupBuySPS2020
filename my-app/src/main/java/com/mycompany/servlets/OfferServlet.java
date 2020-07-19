00359ae7608e912aa39642b9298d875be9beb708
eugene teu <eugeneteu@gmail.com>
2020-07-19 13:58:45 +0800

    deploy to gcloud

--- my-app/src/main/java/com/mycompany/servlets/OfferServlet.java
+++ my-app/src/main/java/com/mycompany/servlets/OfferServlet.java
@@ -82,10 +82,10 @@ public class OfferServlet extends HttpServlet {
         }
     }
 
-//    @Override
-//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String json = gson.toJson(xxx);
-//        response.setContentType("text/html;");
-//        response.getWriter().println(json);
-//    }
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+       //String json = gson.toJson(xxx);
+       response.setContentType("text/html;");
+       response.getWriter().println("Hello World!");
+   }
 }
