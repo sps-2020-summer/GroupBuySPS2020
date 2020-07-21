0cef1a461fdcdea84138a67f17bbff0b275f5cd6
Eugene Teu <eugene.teu@shopee.com>
2020-07-21 20:46:08 +0800

    fix

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,97 +3,93 @@ import Axios from "axios";
 const baseURL = "https://spsteam10-748f8.et.r.appspot.com/";
 
 export const Url = {
-  getTask: () => baseURL + "task/findByStatus",
-  postTask: () => baseURL + "task",
-  getRequest: () => baseURL + "request/findByStatus",
-  postRequest: () => baseURL + "request",
-  //testOffer: () => baseURL + "offer",
-  offer: () => baseURL + "offer",
+	getTask: () => baseURL + "task/findByStatus",
+	postTask: () => baseURL + "task",
+	getRequest: () => baseURL + "request/findByStatus",
+	postRequest: () => baseURL + "request",
+	//testOffer: () => baseURL + "offer",
+	offer: () => baseURL + "offer",
 };
 
 export const getUserRequest = async (uuid: number, status: string) => {
-  return Axios.get(Url.getRequest(), {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    withCredentials: true,
-    params: {
-      id: uuid,
-      status,
-    },
-  });
+	return Axios.get(Url.getRequest(), {
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		withCredentials: true,
+		params: {
+			id: uuid,
+			status,
+		},
+	});
 };
 
 export const getUserTask = async (uuid: number, status: string) => {
-  return Axios.get(Url.offer(), {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    withCredentials: true,
-    params: {
-      uuid: uuid,
-      status,
-      offerId: 1,
-    },
-  });
+	return Axios.get(Url.offer(), {
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+		},
+		withCredentials: true,
+		params: {
+			uuid: 12312312312312312313,
+		},
+	});
 };
 
 export const getOpenOffers = async () => {
-  return Axios.get(Url.offer(), {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    withCredentials: true,
-    params: {
-      status: "open",
-    },
-  });
+	return Axios.get(Url.offer(), {
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		withCredentials: true,
+		params: {
+			status: "open",
+		},
+	});
 };
 
 export const getOpenRequest = async () => {
-  return Axios.get(Url.getRequest(), {
-    withCredentials: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    params: {
-      status: "open",
-    },
-  });
+	return Axios.get(Url.getRequest(), {
+		withCredentials: true,
+		headers: { "Access-Control-Allow-Origin": "*" },
+		params: {
+			status: "open",
+		},
+	});
 };
 
 export const postRequest = async (params) => {
-  return Axios.post(Url.postRequest(), {
-    withCredentials: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    params,
-  });
+	return Axios.post(Url.postRequest(), {
+		withCredentials: true,
+		headers: { "Access-Control-Allow-Origin": "*" },
+		params,
+	});
 };
 
 export const postOffer = async (params) => {
-  return Axios.post(Url.offer(), {
-    withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    params: {
-      uuid: "1231",
-      shopLocation: "dasdsa",
-      expectedDeliveryTime: 123123,
-    },
-  });
+	return Axios.post(Url.offer(), {
+		withCredentials: true,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		params: {
+			uuid: "1231",
+			shopLocation: "dasdsa",
+			expectedDeliveryTime: 123123,
+		},
+	});
 };
 
 export const postTask = async (params) => {
-  return Axios.post(Url.postTask(), {
-    withCredentials: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/x-www-form-urlencoded",
-    
-    },
-    params,
-  });
+	return Axios.post(Url.postTask(), {
+		withCredentials: true,
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		params,
+	});
 };
--- frontend/src/pages/dashboard/user-task/index.tsx
+++ frontend/src/pages/dashboard/user-task/index.tsx
@@ -9,60 +9,61 @@ import Loader from "../../../components/loader";
 import { getUserTask } from "../../../api";
 
 const UserTask: FC<DashboardCompProps> = ({ userUid }) => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [tasks, setTasks] = useState<Task[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [tasks, setTasks] = useState<Task[]>([]);
 
-  useEffect(() => {
-    const fetchTask = async (userUid: number) => {
-      try {
-        setLoading(true);
-        const res = await getUserTask(userUid, "open");
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchTask = async (userUid: number) => {
+			try {
+				setLoading(true);
 
-    fetchTask(Number(userUid));
-  }, []);
+				const res = await getUserTask(userUid, "open");
+			} catch (e) {
+				console.log(e);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-  return (
-    <>
-      <Slide triggerOnce direction={"bottom"}>
-        <h1>My Task</h1>
-        <h3>These are the tasks you are currently doing </h3>
-      </Slide>
-      <br />
-      {loading ? (
-        <Loader spin={loading} topMargin={"24px"} />
-      ) : (
-        <>
-          {tasks.length === 0 ? (
-            <>You currently have no tasks!</>
-          ) : (
-            <List
-              grid={{
-                gutter: 16,
-                xs: 1,
-                sm: 2,
-                md: 4,
-                lg: 4,
-                xl: 4,
-                xxl: 3,
-              }}
-              dataSource={tasks}
-              renderItem={(item) => (
-                <List.Item>
-                  <TaskItem name={item.name} />
-                </List.Item>
-              )}
-            />
-          )}
-        </>
-      )}
-    </>
-  );
+		fetchTask(Number(userUid));
+	}, []);
+
+	return (
+		<>
+			<Slide triggerOnce direction={"bottom"}>
+				<h1>My Task</h1>
+				<h3>These are the tasks you are currently doing </h3>
+			</Slide>
+			<br />
+			{loading ? (
+				<Loader spin={loading} topMargin={"24px"} />
+			) : (
+				<>
+					{tasks.length === 0 ? (
+						<>You currently have no tasks!</>
+					) : (
+						<List
+							grid={{
+								gutter: 16,
+								xs: 1,
+								sm: 2,
+								md: 4,
+								lg: 4,
+								xl: 4,
+								xxl: 3,
+							}}
+							dataSource={tasks}
+							renderItem={(item) => (
+								<List.Item>
+									<TaskItem name={item.name} />
+								</List.Item>
+							)}
+						/>
+					)}
+				</>
+			)}
+		</>
+	);
 };
 
 export default UserTask;
