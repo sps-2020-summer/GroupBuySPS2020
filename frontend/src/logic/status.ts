19115a0eaac283a314a3da29b82ea14ac881da0d
pohlinwei <linwei0909@hotmail.com>
2020-07-22 21:29:52 +0800

    Add offer, utillities

---
+++ frontend/src/logic/status.ts
@@ -0,0 +1,6 @@
+export enum Status {
+    OPEN = "OPEN",
+    PENDING = "PENDING",
+    DONE = "DONE",
+    CANCELLED = "CANCELLED"
+}
---
+++ frontend/src/logic/userlogic.ts
@@ -0,0 +1,3 @@
+import { db } from "./utilities";
+
+export const COLLECTION_USERS: string = "users";
