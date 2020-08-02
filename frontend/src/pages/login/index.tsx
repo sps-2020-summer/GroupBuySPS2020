337b8924ea8e6e167ff9bc634a2681f127747bf4
eugene teu <eugeneteu@gmail.com>
2020-08-03 01:37:14 +0800

    login formating

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,11 +1,11 @@
-import React, { FC, useContext, useState } from 'react'
-import s from './login.module.css'
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import { AuthContext } from '../../context/auth'
-import { FaUser } from 'react-icons/fa'
-import { Spin } from 'antd'
-import { Button } from 'antd'
-import { useHistory } from 'react-router'
+import React, { FC, useContext, useState } from "react"
+import s from "./login.module.css"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import { AuthContext } from "../../context/auth"
+import { FaUser } from "react-icons/fa"
+import { Spin } from "antd"
+import { Button } from "antd"
+import { useHistory } from "react-router"
 
 const Login: FC<{}> = () => {
     const history = useHistory()
@@ -15,8 +15,8 @@ const Login: FC<{}> = () => {
     const [loading, setLoading] = useState<boolean>(false)
 
     const uiConfig = {
-        signInFlow: 'popup',
-        signInSuccessUrl: '/',
+        signInFlow: "popup",
+        signInSuccessUrl: "/",
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
@@ -27,13 +27,13 @@ const Login: FC<{}> = () => {
         setLoading(true)
         const res = await firebase.auth().signInAnonymously()
         console.log(res)
-        history.push('/')
+        history.push("/")
     }
 
     return (
         <div className={s.loginCard}>
-            <FaUser style={{ fontSize: '10vh' }} />
-            <h1 style={{ fontSize: '8vh' }}>Group Buy</h1>
+            <FaUser style={{ fontSize: "10vh" }} />
+            <h1 style={{ fontSize: "8vh" }}>Group Buy</h1>
             <StyledFirebaseAuth
                 uiConfig={uiConfig}
                 firebaseAuth={firebase.auth()}
@@ -42,7 +42,7 @@ const Login: FC<{}> = () => {
                 <Spin spinning={loading}></Spin>
             ) : (
                 <Button
-                    style={{ width: '220px' }}
+                    style={{ width: "220px" }}
                     type="primary"
                     onClick={handleSignIn}
                 >
